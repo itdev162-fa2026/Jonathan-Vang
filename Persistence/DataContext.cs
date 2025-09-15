5dd715be00b3c76018722f813327d9fdd00860a3
Jonathan Vang <Jonathanvang06@gmail.com>
2025-09-14 19:30:04 -0500

    Completed Activity 3

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,20 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class DataContext : DbContext
+{
+    public string DbPath { get; }
+
+    public DbSet<WeatherForecast> WeatherForecasts => Set<WeatherForecast>();
+
+    public DataContext()
+    {
+        var folder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        DbPath = Path.Join(folder, "weather.db"); // local sqlite file
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
