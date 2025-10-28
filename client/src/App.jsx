39769c5df7eb09d6f547abc034027f0e824fca4b
Jonathan Vang <Jonathanvang06@gmail.com>
2025-10-28 17:10:07 -0500

    Complete Activity 7: React Frontend Setup & Product Display

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import ProductDetail from "./components/ProductDetail";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
